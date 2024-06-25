7812d7672c44f098257073bdd96b563965fd5769
Nattaphon Nagasrirung <xenoshop0@gmail.com>
2024-06-25 14:28:19 +0700

    nattaphon_3

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,41 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  weight?: number;
+  height?: number;
+
+  result?: number;
+  result_text?: string = "";
+
+
+
+  constructor() { }
+
+  oncal() {
+    console.log("w :" + this.weight)
+    console.log("h :" + this.height)
+    if (this.weight == undefined || this.height == undefined) {
+      console.log("กรุณากรอกข้อมูลให้ครบถ้วน");
+      return;
+    }
+    var hi = this.height / 100;
+    this.result = Math.floor(this.weight / (hi * hi));
+    console.log("ผลลัพธ์ " + this.result)
+  
+    if (this.result < 18.5) {
+     // console.log("ผอมเกินไป ")
+  this.result_text = "ผอมเกินไป";
+    } else if (this.result >= 18.6 && this.result <= 24) {
+      //console.log("น้ำหนักปกติเหมาะสม");
+      this.result_text = "น้ำหนักปกติเหมาะสม";
+    } else if (this.result >= 25 && this.result <= 29.9) {
+     // console.log("น้ำหนักเกิน");
+     this.result_text = "น้ำหนักเกิน";
+    } else {
+     // console.log("อ้วนมาก");
+     this.result_text = "อ้วนมาก";
+    }
+  }
+  
 
 }
